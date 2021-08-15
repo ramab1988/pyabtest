72a4f69eb0bbdc0f02316ac27549334cd7acd6d7
Rama Badrinath <ramab1988@gmail.com>
2021-08-15 22:55:12 +0530

    fixed variable names

--- src/pyabtest/chi_square.py
+++ src/pyabtest/chi_square.py
@@ -22,14 +22,27 @@ def test_for_binary_metric(
 
 
 def test_for_sample_ratio_mismatch(
-    control_success, control_failures, variant_success, variant_failures, alpha=0.05
+    control_group1_size,
+    control_group2_size,
+    variant_group1_size,
+    variant_group2_size,
+    alpha=0.05,
 ):
-    result = test_for_binary_metric(
-        control_success, control_failures, variant_success, variant_failures, alpha
+
+    table = [
+        [control_group1_size, control_group2_size],
+        [variant_group1_size, variant_group2_size],
+    ]
+    stat, p_value, dof, expected = chi2_contingency(table, correction=False)
+
+    decision = (
+        "Discard A/B test results"
+        if p_value <= alpha
+        else "Don't discard A/B test results"
     )
-    if result["Decision"] == "Reject null hypothesis":
-        decision = "Discard A/B test results"
-    else:
-        decision = "Don't discard A/B test results"
-    result["Decision"] = decision
-    return result
+
+    return {
+        "P-value": round(p_value, 5),
+        "Alpha value (significance level)": alpha,
+        "Decision": decision,
+    }
