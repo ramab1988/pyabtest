a55e9332f05b32ecf7b167288afd95bfe89f20fd
ramab1988 <ramab1988@gmail.com>
2022-06-10 00:55:03 +0530

    doing equality of means test

--- src/pyabtest/bootstrap.py
+++ src/pyabtest/bootstrap.py
@@ -1,55 +1,65 @@
+from numpy import random
 import numpy as np
-from sklearn.utils import resample
-from multiprocessing.pool import ThreadPool
-from multiprocessing import cpu_count
+from numba import jit
 
 
-def get_bootstrapped_data(params):
+@jit(nopython=True, parallel=True, fastmath=True)
+def compute_test_statistic(control_observations, variant_observations):
+    return (np.mean(control_observations) - np.mean(variant_observations)) / np.sqrt(
+        np.var(control_observations) / control_observations.size
+        + np.var(variant_observations) / variant_observations.size
+    )
 
-    data, length, random_number = params
-    sample = resample(data, n_samples=length, random_state=random_number)
-    return np.mean(sample)
+
+@jit(nopython=True, parallel=True, fastmath=True)
+def compute_bootstrap_statistic(
+    control_new_observations, variant_new_observations, no_of_samples
+):
+    bootstrap_statistics = []
+    for _ in range(no_of_samples):
+        bootstrap_statistics.append(
+            compute_test_statistic(
+                np.random.choice(
+                    control_new_observations,
+                    replace=True,
+                    size=len(control_new_observations),
+                ),
+                np.random.choice(
+                    variant_new_observations,
+                    replace=True,
+                    size=len(variant_new_observations),
+                ),
+            )
+        )
+    return np.array(bootstrap_statistics)
 
 
 def test_for_numeric_metric(
     control_observations, variant_observations, alpha=0.05, no_of_samples=10000
 ):
 
-    thread_pool = cpu_count()
-    combined = np.concatenate((control_observations, variant_observations), axis=0)
+    control_observations = np.array(control_observations)
+    variant_observations = np.array(variant_observations)
 
-    bootstrap_control = []
-    bootstrap_variant = []
+    test_statistic = compute_test_statistic(control_observations, variant_observations)
 
-    for i in range(no_of_samples):
-        bootstrap_control.append(
-            (combined, len(control_observations), np.random.randint(i + 1))
-        )
-        bootstrap_variant.append(
-            (combined, len(variant_observations), np.random.randint(i + 1))
-        )
-
-    pool = ThreadPool(thread_pool)
-    bootstrap_control_mean = np.array(
-        pool.map(get_bootstrapped_data, bootstrap_control)
+    combined_observations = np.concatenate(
+        (control_observations, variant_observations), axis=0
     )
-    pool.close()
-    pool.join()
+    combined_mean = np.mean(combined_observations)
 
-    pool = ThreadPool(thread_pool)
-    bootstrap_variant_mean = np.array(
-        pool.map(get_bootstrapped_data, bootstrap_variant)
+    control_new_observations = (
+        control_observations - np.mean(control_observations) + combined_mean
     )
-    pool.close()
-    pool.join()
-
-    dif_bootstrap_means = bootstrap_control_mean - bootstrap_variant_mean
-
-    obs_difs = np.mean(control_observations) - np.mean(variant_observations)
-
-    p_value = (
-        dif_bootstrap_means[dif_bootstrap_means >= obs_difs].shape[0] / no_of_samples
+    variant_new_observations = (
+        variant_observations - np.mean(variant_observations) + combined_mean
     )
+
+    bootstrap_statistics = compute_bootstrap_statistic(
+        control_new_observations, variant_new_observations, no_of_samples
+    )
+
+    p_value = (bootstrap_statistics >= test_statistic).size / no_of_samples
 
     decision = (
         "Reject null hypothesis"
@@ -62,3 +72,4 @@ def test_for_numeric_metric(
         "Alpha value (significance level)": alpha,
         "Decision": decision,
     }
+
